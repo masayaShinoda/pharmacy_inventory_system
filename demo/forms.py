8f7f323556010599c8d82fef650b7c009d37ae14
Masaya Shida <masayashida@gmail.com>
2023-10-02 10:48:04 +0700

    add darkmode toggling, switched to django-tailwind[reload]

--- demo/forms.py
+++ demo/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    input_tailwind_classes = "text-black bg-neutral-200 dark:text-white dark:bg-neutral-600 w-full rounded"
+    input_tailwind_classes = "w-full rounded"
 
     username = forms.CharField(max_length=65, widget=forms.TextInput(attrs={"class": input_tailwind_classes}))
     password = forms.CharField(max_length=65, widget=forms.PasswordInput(attrs={"class": input_tailwind_classes}))
