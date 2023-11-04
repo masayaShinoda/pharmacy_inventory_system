5871bfc692269bddbd5753b33dd60f3ed93c4f7e
Masaya Shida <shida.masaya@tovtrip.com>
2023-11-04 16:11:45 +0700

    minor loginform update

--- demo/forms.py
+++ demo/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    input_tailwind_classes = "w-full rounded py-3 px-2 border border-gray-200"
+    input_tailwind_classes = "text-gray-700 w-full rounded py-3 px-2 border border-gray-200"
 
     username = forms.CharField(max_length=65, widget=forms.TextInput(
         attrs={"class": input_tailwind_classes}))
