d843dd0488e9bd32e1327ef63ce1ec9edc61e6d2
Masaya Shida <shida.masaya@tovtrip.com>
2023-11-12 14:21:33 +0700

    add dialog style

--- demo/forms.py
+++ demo/forms.py
@@ -12,4 +12,4 @@ class AddPharmacyForm(forms.Form):
     input_tailwind_classes = "text-gray-700 w-full rounded my-1 py-2 px-1 border border-gray-200"
  
     name = forms.CharField(max_length=65, widget=forms.TextInput(
-        attrs={"class": input_tailwind_classes}))
+        attrs={"class": input_tailwind_classes, "autofocus": ""}))
